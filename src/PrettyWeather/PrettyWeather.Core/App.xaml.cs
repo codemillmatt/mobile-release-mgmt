6d9e06612e43740d7a0fe5bff81f47d583f77df0
Matt Soucoup <msoucoup@codemilltech.com>
2019-12-02 09:53:03 -0800

    Removing font fixes

--- src/PrettyWeather/PrettyWeather.Core/App.xaml.cs
+++ src/PrettyWeather/PrettyWeather.Core/App.xaml.cs
@@ -1,16 +1,10 @@
-﻿//using Microsoft.AppCenter;
-//using Microsoft.AppCenter.Analytics;
-//using Microsoft.AppCenter.Crashes;
-using PrettyWeather.Pages;
+﻿using PrettyWeather.Pages;
 using Xamarin.Forms;
 
 namespace PrettyWeather
 {
     public partial class App : Application
-    {
-        readonly string iOSAppCenter = "1f555f3f-2d28-4ea6-8242-a017ca8bc1c8";
-        readonly string androidAppCenter = "47db8bf5-4f22-4f2a-ad39-806eee20dfbc";
-
+    {        
         public App()
         {
             InitializeComponent();
@@ -19,11 +13,7 @@ namespace PrettyWeather
         }
 
         protected override void OnStart()
-        {
-            // Handle when your app starts
-            //AppCenter.Start($"ios={iOSAppCenter};android={androidAppCenter};",
-            //    typeof(Analytics), typeof(Crashes));
-
+        {            
             Routing.RegisterRoute("saved-cities", typeof(SavedCitiesPage));
         }
 
